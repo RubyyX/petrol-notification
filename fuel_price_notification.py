b2a319503f75c1dd5431702b33ded880fa0b1b67
Ethan <61816315+RubyyX@users.noreply.github.com>
2023-04-13 11:38:43 +0930

    Update fuel_price_notification.py
    Big edits made over two weeks. Changed some of the logic for communicating with OpenAI API to use improved GPT4 model with more concise and clear query. Added more users and made changes to logic controlling when the code is executed (Wed/Sat) in PythonAnywhere

--- fuel_price_notification.py
+++ fuel_price_notification.py
@@ -1,3 +1,25 @@
+from datetime import datetime
+import pytz
+import sys
+
+desired_day = "Saturday"
+desired_day_2 = "Wednesday"
+
+local_tz = pytz.timezone('Australia/Adelaide')  # Set the timezone to Adelaide
+current_datetime = datetime.now(local_tz)  # Get the current date and time
+current_day = current_datetime.strftime("%A")  # Get the current day
+
+# Format the date and time
+formatted_date = current_datetime.strftime("%d/%m/%Y")
+formatted_time = current_datetime.strftime("%I:%M:%S %p")
+
+# Print the current day and formatted date and time
+print("Current Time is", current_day + " " + formatted_date + " " + formatted_time)
+
+if current_day != desired_day and current_day != desired_day_2:
+    print("Current Time is Invalid, Terminating Program...")
+    sys.exit()
+
 import requests
 from bs4 import BeautifulSoup
 import openai
@@ -18,7 +40,7 @@ def ask_gpt3(system_content, user_content, max_tokens, n=1, stop=None, temperatu
     openai.api_key = "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"
 
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
+        model="gpt-4",
         messages=[
             {
                 "role": "system",
@@ -40,39 +62,55 @@ def ask_gpt3(system_content, user_content, max_tokens, n=1, stop=None, temperatu
 
 
 system_content_1 = "You are a helpful assistant that advises users whether to buy fuel or not based on buying tips."
-user_content_1 = f"""Fuel prices in Adelaide move in cyclic fashion. If prices are high, they will fall, and vice versa. The current buying tip is: \"{adelaide_buying_tip}\". 
+user_content_1 = f"""Fuel prices in Adelaide move in cyclic fashion. If prices are high, they will fall, and vice versa. The current buying tip is: \"{adelaide_buying_tip}\".
 Should I buy or not? It is critical that you only respond with one of two options: 'buy' or 'do not buy', as another program is dependent on the response format."""
 
 # Get response from GPT and remove the expected period from the end
 buying_decision = ask_gpt3(system_content_1, user_content_1, 10, 1, stop=None, temperature=0).strip(".")
 print("GPT Decision: " + buying_decision)
 
-from pushover import init, Client
 import requests
 import json
+import numpy as np
 from statistics import mean
 
-def get_min_avg_fuel_price_by_id(fuel_id):
+def remove_outliers(prices):
+    sorted_prices = sorted(prices)
+    cleaned_prices = [sorted_prices[0]]
+
+    for i in range(1, len(sorted_prices)):
+        if sorted_prices[i] < 5000:
+            cleaned_prices.append(sorted_prices[i])
+
+    return cleaned_prices
+
+def calculate_statistics(prices, percentile):
+    min_price = min(prices)
+    avg_price = round(mean(prices), 2)
+    percentile_price = np.percentile(prices, percentile)
+    return min_price, avg_price, percentile_price
+
+
+def get_prices_by_id(fuel_id):
     url = "https://fppdirectapi-prod.safuelpricinginformation.com.au/Price/GetSitesPrices?countryId=21&geoRegionLevel=2&geoRegionId=189"
     headers = {
         "Authorization": "FPDAPI SubscriberToken=52c12b44-b208-4b74-8109-70a3c2c3aaef",
         "Content-type": "application/json"
     }
     response = requests.get(url, headers=headers)
-    
+
     if response.status_code == 200:
         data = json.loads(response.text)
         prices = [site["Price"] for site in data["SitePrices"] if site["FuelId"] == fuel_id]
         if not prices:
             print(f"No prices found for Fuel ID {fuel_id}")
-            return None, None
-        min_price = min(prices)
-        avg_price = round(mean(prices), 2)
-        return min_price, avg_price
+            return []
+        return prices
     else:
         print(f"Error: Status Code: {response.status_code}")
         print(f"Response Text: {response.text}")
-        return None, None
+        return []
+
 
 def get_fuel_name(fuel_id):
     fuel_id_name_map = {
@@ -100,49 +138,62 @@ users = [
     {
         "name": "Ethan",
         "user_key": "u39w7x6rumuncui32usxk93nmfss43",
-        "preferred_fuel_id": 8  # Unleaded fuel
+        "preferred_fuel_id": 8  # U98
     },
     {
         "name": "Keeley",
         "user_key": "urwdreq9cxn372v8gv5xeh2mozf9n7",
-        "preferred_fuel_id": 2  # Unleaded fuel
+        "preferred_fuel_id": 2  # U91
     },
     {
         "name": "Connor",
         "user_key": "um4gi4nap93rg9jgxjkysfgyi2gdf5",
-        "preferred_fuel_id": 2  # Unleaded fuel
+        "preferred_fuel_id": 2  # U91
     },
     {
         "name": "Tim",
-        "user_key": "usuiknymhiaei7dthsw2wc141m16zi",
-        "preferred_fuel_id": 3  # Unleaded fuel
+        "user_key": "u8ujknymhjaei7dthsw2wc141m16zi",
+        "preferred_fuel_id": 3  # Diesel
+    },
+    {
+        "name": "Stasio",
+        "user_key": "uie55xysyz3xbyx6n39ppcogrmqc81",
+        "preferred_fuel_id": 8  # U98
     }
-    
+
 ]
 
 pushover_app_token = "a5bwbgc248vgcpp7bcuu99jz43v4f7"
 
+# Replace the send_push_notification function with the following:
 def send_push_notification(user_key, message, user_name):
     try:
-        init(pushover_app_token)
-        client = Client(user_key)
-        client.send_message(message, title="Fuel Price Alert")
+        url = "https://api.pushover.net/1/messages.json"
+        payload = {
+            "token": pushover_app_token,
+            "user": user_key,
+            "message": message,
+            "title": "Fuel Price Alert"
+        }
+        response = requests.post(url, data=payload)
+        if response.status_code != 200:
+            print(f"Error sending notification to {user_name}: {response.text}")
     except Exception as e:
         print(f"Error sending notification to {user_name}: {e}")
-    
+
+
 def sentence_case(text):
     return text[0].upper() + text[1:]
 
 def get_concise_buying_tip(prompt):
     response = ask_gpt3(
-        system_content="You are a helpful assistant that rephrases sentences to make men extremely concise and suitable for short push notifications.",
-        user_content=f"""Please make this Adelaide buying tip extremely concise. Ensure it also sounds pleasant to read for a short push notification. Furthermore, ensure
-you use language that makes the notification sound certain about the forecasted price movement, and doesn't use speculative language. Do not label the notification e.g.:
-'Adelaide buying tip: ...', just keep it concise. Keep to 6 words or less. Buying tip: {prompt}""",
-        max_tokens=50,
+        system_content="You are an AI language model providing concise fuel buying tips for push notifications.",
+        user_content=f"""Rewrite the following Adelaide buying tip to be extremely concise, pleasant to read, and convey certainty about the forecasted price movement. Recommend a user action in 8 words or less without labeling the tip.
+Do not use full stops (periods) or quotation marks in the response, and ensure to use setnence case. Original tip: {prompt}""",
+        max_tokens=10,
         n=1,
         stop=None,
-        temperature=0.5,
+        temperature=0,
     )
     message = response.strip()
     return message
@@ -151,13 +202,22 @@ concise_adelaide_buying_tip = get_concise_buying_tip(adelaide_buying_tip)
 adelaide_buying_tip = sentence_case(concise_adelaide_buying_tip).strip(".")
 
 for user in users:
-    min_fuel_price, average_fuel_price = get_min_avg_fuel_price_by_id(user["preferred_fuel_id"])
-    min_fuel_price_moved = min_fuel_price / 10  # Move decimal three places to the left
+    percentile = 5
+    prices = get_prices_by_id(user["preferred_fuel_id"])
+    cleaned_prices = remove_outliers(prices)
+    min_price, avg_price, pctl_price = calculate_statistics(cleaned_prices, percentile)
+
     fuel_name = get_fuel_name(user["preferred_fuel_id"])
-    
-    notification_msg = f"{fuel_name} {min_fuel_price_moved:.1f} | {adelaide_buying_tip}"
+
+    if user["preferred_fuel_id"] == 3:  # Diesel
+        price_to_use = avg_price
+    else:  # Unleaded fuel types
+        price_to_use = pctl_price
+
+    price_to_use_moved = price_to_use / 10  # Move decimal three places to the left
+    notification_msg = f"{fuel_name} {price_to_use_moved:.1f} | {adelaide_buying_tip}"
     print(f"User: {user['name']}, Notification Message: {notification_msg}")
-    
+
     if buying_decision == "buy":
         send_push_notification(user["user_key"], notification_msg, user['name'])
     elif buying_decision == "do not buy":
